9762c5bdabe0d8b32b47c32b4a1029fdf3856ee1
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-10 19:45:35 +0300

    Create 2.3constants.php

---
+++ 2.3constants.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>
+Constante
+</title>
+</head>
+<body>
+TIP de data Constante<br>
+<!-- acesta este un text -->
+<?php
+
+    $var1 = 980;
+    define("MAX", "Eugen Morar");
+    
+    $var1++;
+    
+    echo "1. variabila poate fi incrementata " . $var1 . "<br>";    
+    echo "2. constanta nu " . MAX  . "<br>";   
+
+?>
+</body>
+</html>
