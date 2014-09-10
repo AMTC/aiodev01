5e1b5f7127e9e5441ab71ac8f4fd626321dda757
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-10 20:07:20 +0300

    Create 3.1primapagina.php

---
+++ 3.1primapagina.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>
+URL Values
+</title>
+</head>
+<body>
+Prima Pagina<br>
+<!-- acesta este un text -->
+<a href="adouapagina.php?nume=morar&prenume=eugen&id=3">A doua Pagina</a>
+</body>
+</html>
