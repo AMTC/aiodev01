d62f2a3d8be8cf57cd43d81d932d56484d80ad3d
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-10 20:20:07 +0300

    Create 3.12salut.php

---
+++ 3.12salut.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>
+SALUT 1
+</title>
+</head>
+<body>
+SALUT 1<br>
+<!-- acesta este un text -->
+<?php
+//
+    include("function.php");
+    echo "Aici este functia declarata in fisierul extern" . salut1("Octavian");
+
+?>
+</body>
+</html>
