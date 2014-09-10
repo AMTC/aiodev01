ea06942fe6f62c74d6311b41aa329e4c90436f73
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-10 19:57:27 +0300

    Create 2.10whileloop.php

---
+++ 2.10whileloop.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>
+While
+</title>
+</head>
+<body>
+While loop<br>
+<!-- acesta este un text -->
+<?php
+
+    $var1 = 0;
+    
+    while ( $var1 <= 5 ) {
+	echo $var1 . "salut" . "<br>";
+	$var1++;
+    }
+
+?>
+</body>
+</html>
