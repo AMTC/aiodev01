e3aa8a261799dc9674022a0e65e97a524b4a6bf3
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-10 19:46:55 +0300

    Create 2.5forloop.php

---
+++ 2.5forloop.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>
+FOR
+</title>
+</head>
+<body>
+FOR loop<br>
+<!-- acesta este un text -->
+<?php
+
+    for ( $var1 = 0; $var1 <=5; $var1++ ) {
+	echo $var1 . "salut" . "<br>";
+    }
+
+?>
+</body>
+</html>
