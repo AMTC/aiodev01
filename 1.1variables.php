163b85e784d0f7ca5133d391d3501be6219f2025
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-10 19:26:27 +0300

    Create 1.1variables.php

---
+++ 1.1variables.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>
+Variabile
+</title>
+</head>
+<body>
+Variabile <br>
+<?php
+    $adresa = "STRADA SOVEJA CONSTANTA ";
+    $numar = 41;
+    $numar2 = 10;
+    
+    $Adresa = "strada soveja constanta<br>";
+    echo $adresa . $numar . "<br>";
+    echo $Adresa . "<br>";
+    
+    $rezultat = $numar + $numar2;
+    $rezultat1 = $numar + 10;
+    
+    echo $numar + $numar2 . "<br>";
+    echo $rezultat1 . "<br>";
+
+    $adresa = "STRADA STEFAN<br> ";
+    echo $adresa;
+?>
+</body>
+</html>
