b4c1f1d7ce531c11c01fdcbe3bbd04dc49b5eefc
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-10 20:14:39 +0300

    Create 3.3form.php

---
+++ 3.3form.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>
+FORMULAR
+</title>
+</head>
+<body>
+FORMULAR<br>
+<!-- acesta este un text -->
+<form action="process.php" method="post">
+Username: <input type="text" name="utilizator" value="">
+Password: <input type="password" name="parola" value="">
+<input type="submit" name="buton" value="aici">
+</form>
+</body>
+</html>
