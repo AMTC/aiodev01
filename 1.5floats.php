cd605a73a15b012cb3e90db6eedb634e78d73c76
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-10 19:28:32 +0300

    Create 1.5floats.php

---
+++ 1.5floats.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>
+Variabile
+</title>
+</head>
+<body>
+Numere Floats<br>
+<!-- acesta este un text -->
+<?php
+    $numar = 41.3;
+    echo "1. " . $numar . "<br>";
+    
+    echo "2. " . round($numar) . "<br>";
+    
+    echo "3. " . ceil($numar) . "<br>";
+    
+    echo "4. " . floor($numar) . "<br>";
+    
+    echo "5. " . rand() . "<br>";
+
+    echo "5. " . rand(1,10) . "<br>";
+
+?>
+</body>
+</html>
