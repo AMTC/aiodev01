4d91603d6efe1c0a6dd928d954e16ebebcd8a253
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-10 20:19:48 +0300

    Create 3.11cookies.php

---
+++ 3.11cookies.php
@@ -0,0 +1,18 @@
+<?php
+    
+    setcookie("test","valeriu",time()+(60*60*24*7)); 
+    setcookie("test1","eugen",time()+(60*60*24*7)); 
+    setcookie("test2","octavian",time()+(60*60*24*7)); 
+    setcookie("test3","dan",time()+(60*60*24*7)); 
+?>
+<html>
+<head>
+<title>
+Cookies
+</title>
+</head>
+<body>
+Cookies<br>
+<!-- acesta este un text -->
+</body>
+</html>
